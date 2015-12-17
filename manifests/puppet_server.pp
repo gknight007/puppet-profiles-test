077e32bc65cdc367002aa6bbf7fdc0cdd672f174
Gregory Knight <gregory.knight@vocalocity.com>
2015-12-17 13:29:59 -0500

    test

---
+++ manifests/puppet_server.pp
@@ -0,0 +1,17 @@
+
+class profiles::puppet_server () {
+
+  class { '::puppet': 
+    server => true,
+    server_foreman => false, #Turn off foreman integration
+    server_reports => 'store',
+    server_environments => [], #Do this to override for r10k
+  } 
+
+
+  class { '::r10k':
+    remote  => 'git@github.com:gknight007/test-puppet-control.git',
+    basedir => "${::settings::confdir}/environments",
+  }
+
+}
